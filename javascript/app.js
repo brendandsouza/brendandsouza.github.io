e3cf49d471b81a9994247be716bea56a8dff9820
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-01-07 15:58:29 -0500

    tests

--- javascript/app.js
+++ javascript/app.js
@@ -17,22 +17,35 @@ $(document).ready(function(){
 	//   }
 	// }
 
-	$("#home").click(function(){
-		$("#home").toggleClass("not", addOrRemove);
-		$("#about").toggleClass("not", addOrRemove);
-		$("#resume").toggleClass("not", addOrRemove);
-	});
+	// Get current page URL
+	var url = window.location.href;
 
-	$("#about").click(function(){
-		$("#about").toggleClass("not", addOrRemove);
-		$("#home").toggleClass("not", addOrRemove);
-		$("#resume").toggleClass("not", addOrRemove);
-	});
+	// remove # from URL
+	url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
 
-	$("#resume").click(function(){
-		$("#resume").toggleClass("not", addOrRemove);
-		$("#home").toggleClass("not", addOrRemove);
-		$("#about").toggleClass("not", addOrRemove);
+	// remove parameters from URL
+	url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+
+	// select file name
+	url = url.substr(url.lastIndexOf("/") + 1);
+
+	// If file name not avilable
+	if(url == ''){
+	url = 'index.html';
+	}
+
+	// Loop all menu items
+	$('.navbar').each(function(){
+
+	// select href
+	var href = $(this).find('a').attr('href');
+
+	// Check filename
+	if(url == href){
+
+		// Add active class
+		$(this).addClass('active');
+	}
 	});
 });
 
