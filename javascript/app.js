d647235d3203e0ba21bbd184dea7b98a8dfd8782
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-01-07 14:54:00 -0500

    navbar tests

--- javascript/app.js
+++ javascript/app.js
@@ -1,21 +1,39 @@
-//$(document).ready(function(){
-	// When the user scrolls the page, execute myFunction 
-	window.onscroll = function() {myFunction()};
+$(document).ready(function(){
+	// // When the user scrolls the page, execute myFunction 
+	// window.onscroll = function() {myFunction()};
 
-	// Get the navbar
-	var navbar = document.getElementById("navbar");
+	// // Get the navbar
+	// var navbar = document.getElementById("navbar");
 
-	// Get the offset position of the navbar
-	var sticky = navbar.offsetTop;
+	// // Get the offset position of the navbar
+	// var sticky = navbar.offsetTop;
 
-	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-	function myFunction() {
-	  if (window.pageYOffset >= sticky) {
-	    navbar.classList.add("sticky")
-	  } else {
-	    navbar.classList.remove("sticky");
-	  }
-	}
-//});
+	// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+	// function myFunction() {
+	//   if (window.pageYOffset >= sticky) {
+	//     navbar.classList.add("sticky")
+	//   } else {
+	//     navbar.classList.remove("sticky");
+	//   }
+	// }
+
+	$("#home").click(function(){
+		$("#home").toggleClass("active");
+		$("#about").toggleClass("inactive");
+		$("#resume").toggleClass("inactive");
+	});
+
+	$("#about").click(function(){
+		$("#about").toggleClass("active");
+		$("#home").toggleClass("inactive");
+		$("#resume").toggleClass("inactive");
+	});
+
+	$("#resume").click(function(){
+		$("#resume").toggleClass("active");
+		$("#home").toggleClass("inactive");
+		$("#about").toggleClass("inactive");
+	});
+});
 
 
