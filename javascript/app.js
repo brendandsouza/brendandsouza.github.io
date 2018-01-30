b1218552f67c804b546740ceb688b3c275c159c3
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-01-29 22:11:41 -0500

    s

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,7 @@ var app = angular.module("main", []);
 
 var mainController = function($scope){
     $scope.message = "dhelloooo";
+    $scope.searchitem = "test";
 };
 
 app.controller("mainController", mainController);
