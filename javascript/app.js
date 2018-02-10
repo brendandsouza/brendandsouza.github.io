0e5fd79d28281b83f3bbb67f2b4b7728a4d6ce54
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-02-10 15:08:33 -0500

    added scroll functionality

--- javascript/app.js
+++ javascript/app.js
@@ -6,4 +6,24 @@ var mainController = function($scope){
     $scope.searchitem = "test";
 };
 
+var scrollOnClick = function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top}, "slow");
+      });
+    }
+  }
+}
+
 app.controller("mainController", mainController);
+
+app.directive("scrollOnClick", scrollOnClick);
