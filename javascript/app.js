8771323c5fe48c52b33ee3be2fdb48abfa6cd1b3
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-02-16 23:08:55 -0500

    p

--- javascript/app.js
+++ javascript/app.js
@@ -6,30 +6,30 @@ var mainController = function($scope){
     $scope.searchitem = "test";
 };
 
-// var scrollOnClick = function() {
-//   return {
-//     restrict: 'A',
-//     link: function(scope, $elm, attrs) {
-//       var idToScroll = attrs.href;
-//       $elm.on('click', function() {
-//         var target;
-//         if (idToScroll) {
-//           target = $(idToScroll);
-//         } else {
-//           target = $($elm);
-//         }
-//         $('html, body').animate({scrollTop: $(target).offset().top}, "slow");
-//       });
-//     }
-//   }
-// }
+var scrollOnClick = function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var target;
+        if (idToScroll) {
+          target = $(idToScroll);
+        } else {
+          target = $($elm);
+        }
+        $("body").animate({scrollTop: $(target).offset().top}, "slow");
+      });
+    }
+  }
+}
 
 app.controller("mainController", mainController);
 
-// app.directive("scrollOnClick", scrollOnClick);
-$("#scroll-on-click > a").click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, slow);
-    return false;
-});
+app.directive("scrollOnClick", scrollOnClick);
+// $("#scroll-on-click > a").click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top
+//     }, slow);
+//     return false;
+// });
