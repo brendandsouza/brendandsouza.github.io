8d8c043e88152096e502ee41dcc93da1ca72e6b2
Brendan D'Souza <brendan_dsouza@hotmail.com>
2018-02-17 21:01:06 -0500

    added angular

--- javascript/app.js
+++ javascript/app.js
@@ -1,45 +1,45 @@
 
-// var app = angular.module("main", []);
+var app = angular.module("main", []);
 
-// var mainController = function($scope){
-//     $scope.message = "dhelloooo";
-//     $scope.searchitem = "test";
-// };
+var mainController = function($scope){
+    $scope.message = "dhelloooo";
+    $scope.searchitem = "test";
+};
 
-// var scrollOnClick = function() {
-//   return {
-//     restrict: 'A',
-//     link: function(scope, $elm, attrs) {
-//       var idToScroll = attrs.href;
-//       $elm.on('click', function() {
-//         var target;
-//         if (idToScroll) {
-//           target = $(idToScroll);
-//         } else {
-//           target = $($elm);
-//         }
-//         $("html, body").animate({scrollTop: $(target).offset().top}, "slow");
-//       });
-//     }
-//   }
-// }
-
-// app.controller("mainController", mainController);
-
-// app.directive("scrollOnClick", scrollOnClick);
-
-function scrollNav() {
-  $('.nav a').click(function(){  
-    //Toggle Class
-    $(".active").removeClass("active");      
-    $(this).addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('a').addClass('active');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
-    }, 400);
-    return false;
-  });
+var scrollOnClick = function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var target;
+        if (idToScroll) {
+          target = $(idToScroll);
+        } else {
+          target = $($elm);
+        }
+        $("html, body").animate({scrollTop: $(target).offset().top}, "slow");
+      });
+    }
+  }
 }
-scrollNav();
+
+app.controller("mainController", mainController);
+
+app.directive("scrollOnClick", scrollOnClick);
+
+// function scrollNav() {
+//   $('.nav a').click(function(){  
+//     //Toggle Class
+//     $(".active").removeClass("active");      
+//     $(this).addClass("active");
+//     var theClass = $(this).attr("class");
+//     $('.'+theClass).parent('a').addClass('active');
+//     //Animate
+//     $('html, body').stop().animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top - 160
+//     }, 400);
+//     return false;
+//   });
+// }
+// scrollNav();
